a37ace06eabd1f018526156d9f49f2aaafdcf40c
Kevin Diesenberg <kdiesenb@gmail.com>
2023-01-10 11:42:33 -0500

    working on bonus. use byebug

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,2 +1,16 @@
 class StudentsController < ApplicationController
+    def index
+        # byebug
+        students = Student.all
+        render json: students
+    end
+
+    def grades
+        students = Student.order(grade: :desc)
+        # english...order the whole Student class by grade in descending order, highest to lowest
+        render json: students
+        # byebug
+    end
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+
   get 'students', to: "students#index"
-  
+  get 'students/grades', to: "students#grades"
+
 end
